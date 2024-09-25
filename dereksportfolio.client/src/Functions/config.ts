b8ef28e4c2cbfe6ee7e890ed38f5f0c2a1d7eb8b
unknown <gaugerderek@gmail.com>
2024-09-25 10:28:21 -0500

    reverting api url

--- dereksportfolio.client/src/Functions/config.ts
+++ dereksportfolio.client/src/Functions/config.ts
@@ -1,2 +1,2 @@
-export const API_URL = 'http://localhost:5084/api';
-// export const API_URL = 'https://derekgauger.com/api';
+// export const API_URL = 'http://localhost:5084/api';
+export const API_URL = 'https://derekgauger.com/api';
