52c5fca052e5c7d98fb215e7f2f8630c86b74758
unknown <gaugerderek@gmail.com>
2024-09-29 22:07:16 -0500

    Fixing privacy policy

--- dereksportfolio.client/src/Pages/TruthPrivacy/TruthPrivacy.tsx
+++ dereksportfolio.client/src/Pages/TruthPrivacy/TruthPrivacy.tsx
@@ -1,86 +1,18 @@
-// import React from "react";
-// import PageHeader from "../../Components/PageHeader/PageHeader";
-
-// const TruthPrivacy: React.FC = () => {
-//   const containerStyles = "container mx-auto px-4 sm:px-6 lg:px-8";
-//   const headingStyles = "text-green-400 text-2xl sm:text-3xl font-bold mt-6";
-//   const paragraphStyles = "mt-4 text-sm sm:text-base leading-relaxed";
-//   const listItemStyles = "mt-2 text-sm sm:text-base leading-relaxed";
-//   const linkStyles = "text-green-400 hover:text-pink-600 underline";
-
-//   return (
-//     <div className={containerStyles}>
-//       <PageHeader
-//         title="Contact"
-//         description="Welcome to my contact page! Please send me an email. I setup my own email service for this website and would love for it to be used. Include any feedback or questions you have about me or the website."
-//       />
-//       <h1>PRIVACY POLICY</h1>
-//       <p>Last updated March 03, 2024</p>
-//       <p>
-//         This privacy notice for Truth ("we," "us," or "our"), describes how and
-//         why we might collect, store, use, and/or share ("process") your
-//         information when you use our services ("Services"), such as when you:
-//         Questions or concerns? Reading this privacy notice will help you
-//         understand your privacy rights and choices. If you do not agree with our
-//         policies and practices, please do not use our Services. If you still
-//         have any questions or concerns, please contact us at
-//         gaugerderek@gmail.com.
-//       </p>
-//       <h2>SUMMARY OF KEY POINTS</h2>
-//       <p>
-//         This summary provides key points from our privacy notice, but you can
-//         find out more details about any of these topics by clicking the link
-//         following each key point or by using our table of contents below to find
-//         the section you are looking for.
-//       </p>
-//       <p>
-//         What personal information do we process? When you visit, use, or
-//         navigate our Services, we may process personal information depending on
-//         how you interact with us and the Services, the choices you make, and the
-//         products and features you use. Learn more about personal information you
-//         disclose to us.
-//       </p>
-//       <p>
-//         Do we process any sensitive personal information? We do not process
-//         sensitive personal information.
-//       </p>
-//       <p>
-//         Do we receive any information from third parties? We do not receive any
-//         information from third parties.
-//       </p>
-//       <p>
-//         How do we process your information? We process your information to
-//         provide, improve, and administer our Services, communicate with you, for
-//         security and fraud prevention, and to comply with law. We may also
-//         process your information for other purposes with your consent. We
-//         process your information only when we have a valid legal reason to do
-//         so. Learn more about how we process your information.
-//       </p>
-//       <p>
-//         In what situations and with which parties do we share personal
-//         information? We may share information in specific situations and with
-//         specific third parties. Learn more about when and with whom we share
-//         your personal information.
-//       </p>
-//       <p>
-//       What are your rights? Depending on where you are located geographically, the applicable privacy law may mean you have certain rights regarding your personal information. Learn more about your privacy rights.
-
-//       </p>
-//     </div>
-//   );
-// };
-
-// export default TruthPrivacy;
 import React from "react";
 import PageHeader from "../../Components/PageHeader/PageHeader";
 import Footer from "../../Components/Footer/Footer";
 
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace JSX {
+    interface IntrinsicElements {
+      bdt: React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+    }
+  }
+}
+
 const TruthPrivacy: React.FC = () => {
   const containerStyles = "container mx-auto px-4 sm:px-6 lg:px-8";
-  const headingStyles = "text-green-400 text-2xl sm:text-3xl font-bold mt-6";
-  const paragraphStyles = "mt-4 text-sm sm:text-base leading-relaxed";
-  const listItemStyles = "mt-2 text-sm sm:text-base leading-relaxed";
-  const linkStyles = "text-green-400 hover:text-pink-600 underline";
 
   return (
     <div className={containerStyles}>
