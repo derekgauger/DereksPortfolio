06e8982a5941d9733d2fe1d5a66904f564678692
unknown <gaugerderek@gmail.com>
2024-09-29 19:46:25 -0500

    Adding privacy policy, tos, and details page data always

--- DereksPortfolio.Server/obj/Debug/net8.0/DereksPortfolio.Server.AssemblyInfo.cs
+++ DereksPortfolio.Server/obj/Debug/net8.0/DereksPortfolio.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DereksPortfolio.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b6b1b5a60f509770a1ca7f407ccfb602498e5ad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45860db16a49718d8113313f217872194407d70b")]
 [assembly: System.Reflection.AssemblyProductAttribute("DereksPortfolio.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DereksPortfolio.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
